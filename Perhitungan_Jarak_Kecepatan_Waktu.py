192401dba76159a043b49dcd30585248ceaa60c4
Pasha_adiyatma <pashaadiyatma101007@gmail.com>
2025-12-24 10:41:07 +0700

    first commit

---
+++ Perhitungan_Jarak_Kecepatan_Waktu.py
@@ -0,0 +1,40 @@
+def Kecepatan():
+  Jarak = int(input("Masukkan Jarak :"))
+  Waktu = int(input("Masukkan Waktu :"))
+
+  Kecepatan = Jarak / Waktu
+  kecepatan = int(Kecepatan)
+  print(kecepatan)
+
+def Jarak():
+  Kecepatan = int(input("Masukkan Kecepatan :"))
+  Waktu = int(input("Masukkan Waktu :"))
+
+  Jarak = Kecepatan * Waktu
+  jarak = int(Jarak)
+  print(jarak)
+
+def Waktu():
+  Jarak = int(input("Masukkan Jarak :"))
+  Kecepatan = int(input("Masukkan Kecepatan :"))
+
+  Waktu = Jarak / Kecepatan
+  waktu = int(Waktu)
+  print(waktu)
+
+while True :
+  print("===== Perhitungan Kecepatan,Jarak Dan Waktu =====")
+
+  pilihan_user = input("Masukkan Pilihan:")
+  if pilihan_user == "Kecepatan":
+    Kecepatan()
+  elif pilihan_user == "Jarak":
+    Jarak()
+  elif pilihan_user == "Waktu":
+    Waktu()
+  else:
+    exit()
+
+
+
+
