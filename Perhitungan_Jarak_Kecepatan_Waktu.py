5d1123e3e7df52f6cc1838dae2c2786e478eeb04
Pasha_adiyatma <pashaadiyatma101007@gmail.com>
2025-12-24 17:29:49 +0700

    Perubahan Modul
    Refactor speed, distance, and time calculations to use imported functions.

--- Perhitungan_Jarak_Kecepatan_Waktu.py
+++ Perhitungan_Jarak_Kecepatan_Waktu.py
@@ -1,26 +1,8 @@
-def Kecepatan():
-  Jarak = int(input("Masukkan Jarak :"))
-  Waktu = int(input("Masukkan Waktu :"))
+import Rumus 
 
-  Kecepatan = Jarak / Waktu
-  kecepatan = int(Kecepatan)
-  print(f"Kecepatannya Adalah : {kecepatan}")
-
-def Jarak():
-  Kecepatan = int(input("Masukkan Kecepatan :"))
-  Waktu = int(input("Masukkan Waktu :"))
-
-  Jarak = Kecepatan * Waktu
-  jarak = int(Jarak)
-  print(f"Jaraknya Adalah : {jarak}")
-
-def Waktu():
-  Jarak = int(input("Masukkan Jarak :"))
-  Kecepatan = int(input("Masukkan Kecepatan :"))
-
-  Waktu = Jarak / Kecepatan
-  waktu = int(Waktu)
-  print(f"Waktunya Adalah : {waktu}")
+Kecepatan = Rumus.Kecepatan()
+Jarak = Rumus.Jarak()
+Waktu = Rumus.Waktu()
 
 while True :
   print("===== Perhitungan Kecepatan,Jarak Dan Waktu =====")
